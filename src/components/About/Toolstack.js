29839626b118ebc82e3ac6fa21ac425beaa5e7a3
gijigeorge77 <gijigeorge77@gmail.com>
2022-03-29 16:39:35 +0300

    Source Code
    Portfolio

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { SiVisualstudiocode, SiPostman, SiHeroku } from "react-icons/si";
+import { DiGithub, DiWindows } from "react-icons/di";
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
