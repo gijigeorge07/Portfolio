e6d508affc5895ef5133032dcf6c172ff39cf649
gijigeorge77 <gijigeorge77@gmail.com>
2022-03-29 17:40:08 +0300

    source code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,11 +94,11 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumya-jit.tech/"
+                href="https://instagram.com/artworld_bh/"
                 target="_blank"
                 rel="noreferrer"
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+                <ImBlog style={{ marginBottom: "2px" }} /> Hobbies
               </Nav.Link>
             </Nav.Item>
 
